29011834af4fc48ae85597598abcf621ba30276f
Timevolt01 <jayantpotdar2006@gmail.com>
2024-11-08 23:59:30 +0530

    Done

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import time
 import random
 
 pygame.font.init()
+pygame.mixer.init()
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-BORDER = pygame.Rect(WIDTH / 2 - 5, 0, 10, HEIGHT)
+BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 8, HEIGHT)
 
-FONT = pygame.font.SysFont("comicsans", 30)
+FONT = pygame.font.SysFont("comicsans", 80)
+FONT1 = pygame.font.SysFont("comicsans", 30)
 SPACECRAFT_WIDTH, SPACECRAFT_HEIGHT = 60, 55
 
 FPS = 60
 VEL = 5
 BULLET_VEL = 7
-MAX_BULLETS = 3
+MAX_BULLETS = 6
+
+BULLET_HIT_SOUND = pygame.mixer.Sound("./Assets/Grenade+1.mp3")
+BULLET_FIRE_SOUND = pygame.mixer.Sound("./Assets/Gun+Silencer.mp3")
+
+BULLET = []
 
 VIOLET_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
@@ -23,6 +30,7 @@ RED_HIT = pygame.USEREVENT + 2
 VIOLET_SPACECRAFT_IMG = pygame.image.load("./Assets/spaceship_violet.png")
 RED_SPACECRAFT_IMG = pygame.image.load("./Assets/spaceship_red.png")
 
+BG = pygame.transform.scale(pygame.image.load("./Assets/space.png"), (WIDTH, HEIGHT))
 
 VIOLET_SPACECRAFT_TRANSFORM = pygame.transform.rotate(
     pygame.transform.scale(
@@ -38,13 +46,38 @@ RED_SPACECRAFT_TRANSFORM = pygame.transform.rotate(
 pygame.display.set_caption("Galaxy Fighter")
 
 
-def drawWindows(VIOLET_SPACECRAFT, RED_SPACECRAFT):
-    WIN.fill("white")
+def drawWindows(
+    VIOLET_SPACECRAFT,
+    RED_SPACECRAFT,
+    red_bullets,
+    violet_bullets,
+    red_health,
+    violet_health,
+):
+    WIN.blit(BG, (0, 0))
+
+    pygame.draw.rect(WIN, "black", BORDER)
 
     WIN.blit(VIOLET_SPACECRAFT_TRANSFORM, (VIOLET_SPACECRAFT.x, VIOLET_SPACECRAFT.y))
     WIN.blit(RED_SPACECRAFT_TRANSFORM, (RED_SPACECRAFT.x, RED_SPACECRAFT.y))
 
-    pygame.draw.rect(WIN, "black", BORDER)
+    for bullet in red_bullets:
+        pygame.draw.rect(WIN, "red", bullet)
+
+    for bullet in violet_bullets:
+        pygame.draw.rect(WIN, "blue", bullet)
+
+    red_health_text = FONT1.render("Health: " + str(red_health), 1, "White")
+    violet_health_text = FONT1.render("Health: " + str(violet_health), 1, "White")
+    WIN.blit(red_health_text, (
+            WIDTH - red_health_text.get_width() - 20,
+            HEIGHT - violet_health_text.get_height() - 10,
+        ),)
+    WIN.blit(
+        violet_health_text,
+        (5, HEIGHT - red_health_text.get_height() - 10)
+    )
+
     pygame.display.update()
 
 
@@ -74,20 +107,25 @@ def keyBehaviour(keys, VIOLET_SPACECRAFT, RED_SPACECRAFT):
     ):
         VIOLET_SPACECRAFT.y += VEL
 
+
 def bulletMovement(violet_bullets, red_bullets, VIOLET_SPACECRAFT, RED_SPACECRAFT):
+
     for bullet in violet_bullets:
-        bullet.x += BULLET_VEL
+        bullet.x -= BULLET_VEL
         if RED_SPACECRAFT.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(pygame.QUIT))
-        elif bullet.x > WIDTH:
+            pygame.event.post(pygame.event.Event(RED_HIT))
+            violet_bullets.remove(bullet)
+        elif bullet.x < 0:
             violet_bullets.remove(bullet)
 
     for bullet in red_bullets:
-        bullet.x -= BULLET_VEL
+        bullet.x += BULLET_VEL
         if VIOLET_SPACECRAFT.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(pygame.QUIT))
-        elif bullet.x < 0:
-            red_bullets.remove(bullet) 
+            pygame.event.post(pygame.event.Event(VIOLET_HIT))
+            red_bullets.remove(bullet)
+        elif bullet.x > WIDTH:
+            red_bullets.remove(bullet)
+
 
 def main():
 
@@ -101,38 +139,91 @@ def main():
     violet_bullets = []
     red_bullets = []
 
+    violet_health = int(100)
+    red_health = int(100)
+
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LCTRL and len(violet_bullets) < MAX_BULLETS:
-                    violet_bullets.append(
-                        pygame.Rect(
-                            VIOLET_SPACECRAFT.x,
-                            VIOLET_SPACECRAFT.y + VIOLET_SPACECRAFT.height / 2,
-                            10,
-                            5,
-                        )
+                if event.key == pygame.K_RCTRL and len(violet_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        VIOLET_SPACECRAFT.x,
+                        VIOLET_SPACECRAFT.y + VIOLET_SPACECRAFT.height / 2,
+                        10,
+                        5,
                     )
+                    violet_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
 
-                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
-                    red_bullets.append(
-                        pygame.Rect(
-                            RED_SPACECRAFT.x + RED_SPACECRAFT.width,
-                            RED_SPACECRAFT.y + RED_SPACECRAFT.height / 2,
-                            10,
-                            5,
-                        )
+                if event.key == pygame.K_LCTRL and len(red_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        RED_SPACECRAFT.x + RED_SPACECRAFT.width,
+                        RED_SPACECRAFT.y + RED_SPACECRAFT.height / 2,
+                        10,
+                        5,
                     )
+                    red_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
+                    
+            if red_health <= 0 and violet_health <= 0:
+                lost = FONT.render("Draw!", 1, "white")
+                WIN.blit(
+                    lost,
+                    (
+                        WIDTH / 2 - lost.get_width() / 2,
+                        HEIGHT / 4 - lost.get_height() / 2,
+                    ),
+                )
+                pygame.display.update()
+                pygame.time.delay(3000)
+                run = False
+            if event.type == VIOLET_HIT:
+                red_health -= 25
+                BULLET_HIT_SOUND.play()
+                if red_health <= 0:
+                    lost = FONT.render("Red Wins!", 1, "red")
+                    WIN.blit(
+                        lost,
+                        (
+                            WIDTH / 2 - lost.get_width() / 2,
+                            HEIGHT / 4 - lost.get_height() / 2,
+                        ),
+                    )
+                    pygame.display.update()
+                    pygame.time.delay(3000)
+                    run = False
+                print("Violet Got Hit ", red_health)
+
+            if event.type == RED_HIT:
+                violet_health -= 25
+                BULLET_HIT_SOUND.play()
+                if violet_health <= 0: 
+                    lost = FONT.render("Blue Wins!", 1, "Blue")
+                    WIN.blit(
+                        lost,
+                        (
+                            WIDTH / 2 - lost.get_width() / 2,
+                            HEIGHT / 4 - lost.get_height() / 2,
+                        ),
+                    )
+                    pygame.display.update()
+                    pygame.time.delay(3000)
+                    run = False
+
+                print("Red Got Hit ", violet_health)
 
         pygame.display.update()
 
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_ESCAPE] or keys[pygame.K_LCTRL] and keys[pygame.K_w] or BORDER.collidepoint(VIOLET_SPACECRAFT.x, VIOLET_SPACECRAFT.y) or BORDER.collidepoint(RED_SPACECRAFT.x + 50,RED_SPACECRAFT.y):
+        if (
+            keys[pygame.K_ESCAPE]
+            or BORDER.collidepoint(VIOLET_SPACECRAFT.x, VIOLET_SPACECRAFT.y)
+            or BORDER.collidepoint(RED_SPACECRAFT.x + 50, RED_SPACECRAFT.y)
+        ):
             run = False
 
         # ------------------------------- keyBehaviour ------------------------------- #
@@ -141,12 +232,19 @@ def main():
 
         # -------------------------------- drawWindows ------------------------------- #
 
-        drawWindows(VIOLET_SPACECRAFT, RED_SPACECRAFT)
+        drawWindows(
+            VIOLET_SPACECRAFT,
+            RED_SPACECRAFT,
+            red_bullets,
+            violet_bullets,
+            red_health,
+            violet_health,
+        )
 
         # -------------------------------- BulletsPhysics ------------------------------ #
-        
-        bulletMovement(violet_bullets, red_bullets , VIOLET_SPACECRAFT, RED_SPACECRAFT)
-        
+
+        bulletMovement(violet_bullets, red_bullets, VIOLET_SPACECRAFT, RED_SPACECRAFT)
+
     pygame.quit()
 
 
